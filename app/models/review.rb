b32e9c5a13307cb9c1c0793476a15b742fa509d1
Alison Clare <alisonclare93@gmail.com>
2021-11-10 14:21:28 +0000

    created models

---
+++ app/models/review.rb
@@ -0,0 +1,7 @@
+class Review < ApplicationRecord
+  belongs_to :restaurant
+
+  validates :content, presence: true
+  validates :rating, presence: true, inclusion: { in: (0..5),
+    message: 'Rating must be between 0 and 5' }, numericality: { only_integer: true }
+end
